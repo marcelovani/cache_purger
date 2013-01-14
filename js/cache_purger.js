d40c3b7681806612330841dd0a72afcb6f55a26f
root <marcellovani@yahoo.co.uk>
2013-01-14 14:17:12 +0000

    Commiting module

---
+++ js/cache_purger.js
@@ -0,0 +1,37 @@
+(function ($) {
+
+Drupal.behaviors.blockPurge = {
+ attach: function (context) {
+	var block_ids = Drupal.settings.cache_purger;
+	jQuery.each(block_ids.module, function (i, item) {
+		var module = block_ids.module[i];
+		var delta = block_ids.delta[i];
+		var purge_btn = '<div class="cache-purge-btn" title="Purge Cache: ' + module + '-' + delta + '" id="cache-purge-btn-' + module + '-' + delta +	'" onclick="cache_purge_block(\'' + module + '\', \'' + delta + '\');"></div>';
+		jQuery('#block-' + module + '-' + delta + '').append(purge_btn);
+   });
+ }
+};
+
+})(jQuery);
+
+function cache_purge_block(module, delta) {
+	var cssPrefix = false;
+	if (jQuery.browser.webkit) cssPrefix = "webkit";
+	if (jQuery.browser.safari) cssPrefix = "webkit";
+	if (jQuery.browser.mozilla) cssPrefix = "moz";
+	if (jQuery.browser.opera) cssPrefix = "o";
+	if (jQuery.browser.msie) cssPrefix = "ms";
+		
+	if(cssPrefix != false) {
+		  var purge_btn = document.getElementById('cache-purge-btn-' + module + '-' + delta), degrees = 0, speed = 5;
+		  var spinner = setInterval(function() {
+		    degrees += speed; // degree adjustment each interval
+		    purge_btn.setAttribute("style","-" + cssPrefix + "-transform:rotate(" + degrees + "deg)");
+		  },5);
+	}
+	jQuery.getJSON("/admin/config/development/cachepurger/block/" + module + "/" + delta, {}, function(data) {
+		clearInterval(spinner);
+		alert(data.result);
+		jQuery('#cache-purge-btn-' + module + '-' + delta).fadeOut('slow');
+	});
+}
